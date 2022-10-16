919122d565941ae6bb0ef64f357dee064baadc1f
Aashiparihar07 <115942226+Aashiparihar07@users.noreply.github.com>
2022-10-16 22:40:54 +0530

    added new code of bubble sort

---
+++ bubblesort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,temp;
+    int a[20];
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    cout<<"Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<(n-1);i++)
+    {
+        for (int j = 0; j < (n-i-1); j++)
+        {
+            if (a[j]<a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }  
+    }
+     cout<<"Your sorted array is :";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
