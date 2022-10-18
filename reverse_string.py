02a92fd979d3a172d0cf5e0b3dbe9122ca7af16d
Neelesh Pandey <iamsteverogersakacap@gmail.com>
2022-10-18 11:35:13 +0530

    main changes

--- reverse_string.py
+++
@@ -1,9 +0,0 @@
-def reverse_string(str):  
-    str1 = ""   # Declaring empty string to store the reversed string  
-    for i in str:       
-        str1 = i + str1  
-    return str1    # It will return the reverse string to the caller function  
-     
-str = "JavaTpoint"    # Given String       
-print("The original string is: ",str)  
-print("The reverse string is",reverse_string(str)) # Function call 
